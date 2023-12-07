d9b233625f13235fa6151936c88f0b354ded0a3c
Zein Shhadi <zeinshhadi@hotmail.com>
2023-12-07 15:10:29 +0200

    added tokens for create , update, and delete

--- backend/api/products/delete_product.php
+++ backend/api/products/delete_product.php
@@ -1,6 +1,31 @@
 <?php
 include('../../connection.php');
+require __DIR__.'../../../vendor/autoload.php';
+use Firebase\JWT\JWT;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\Key;
 
+$headers=getallheaders();
+
+if(!isset($headers['Authorization']) || empty($headers['Authorization']) ){
+    http_response_code(401);
+    echo json_encode(['error'=>'unauthorized']);
+    exit();
+}
+
+$authorizationHeader = $headers['Authorization'];
+$token=null;
+$token = trim(str_replace("Bearer",'',$authorizationHeader));
+if(!$token){
+        http_response_code(401);
+    echo json_encode(['error'=>'expired']);
+    exit();
+}
+
+try {
+    $key = 'secret_key';
+    $decoded = JWT::decode($token,new Key($key,'HS256'));
+    if($decoded->role_id==1){
 
 $product_id= $_POST['product_id'];
 
@@ -16,3 +41,18 @@ echo 'deleting failed';
 
 $query->close();
 $mysqli->close();
+}else{
+    $response=[];
+   $response["permissions"] =false;
+    echo json_encode($response);
+}
+   
+
+} catch (ExpiredException $e) {
+      http_response_code(401);
+      echo json_encode(['error'=>'unauthorized']);
+}
+
+
+
+
